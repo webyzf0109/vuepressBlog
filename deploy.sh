560f8345f4c99d32eec04d2b3f83041f5f3038f7
webyzf0109@163.com <webyzf0109@163.com>
2020-01-07 17:59:40 +0800

    vuepress

--- deploy.sh
+++ deploy.sh
@@ -3,19 +3,12 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-# 生成静态文件
-npm run build
-
-# 进入生成的文件夹
-cd docs/.vuepress/dist
-
-
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m 'vuepress'
 
 # 如果你想要部署到 https://USERNAME.github.io
-git push -f git@github.com:webyzf0109/webyzf0109.github.io.git master
+git push -f git@github.com:webyzf0109/vuepressBlog.git master
 
 # 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
 # git push -f git@github.com:USERNAME/<REPO>.git master:gh-pages
